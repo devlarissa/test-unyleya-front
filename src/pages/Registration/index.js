3391d5deebf96fcf14e5bdb44a6d92a656aa976f
Larissa Ramos <devlarissase@gmail.com>
2019-11-30 21:40:34 -0300

    register screen

---
+++ src/pages/Registration/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import _Page from "../_Page";
+import { Button, Card, Input, Icon, Avatar } from "antd";
+import "./styles.css";
+import Logo from "../../assets/img/logobo.png";
+
+class Registration extends _Page {
+  render() {
+    return (
+      <>
+        <div className="body">
+          <Card title="Cadastro" className="card" extra={<Icon type="rollback"/>}>
+          <div className="header">
+            <Avatar
+              src={Logo}
+              shape="square"
+              style={{ width: "30%", height: "30%" }}
+            />
+            <p>
+                Desculpa, no momento não estamos realizando mais cadastros. Em breve voltaremos com atualizações.
+            </p>
+            </div>
+          </Card>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Registration;
